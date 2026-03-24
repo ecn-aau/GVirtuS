7982df5504672f94a0f4f99fcb2b127dba549d63
Vasco Afonso <vafons25@student.aau.dk>
2026-03-24 10:31:11 +0100

    Import quic changes from https://gitlab.com/ralfluebben/GVirtuS/-/tree/quic-merge

--- src/communicators/EndpointFactory.cpp
+++ src/communicators/EndpointFactory.cpp
@@ -1,3 +1,4 @@
 #include "gvirtus/communicators/EndpointFactory.h"
 
 int gvirtus::communicators::EndpointFactory::ind_endpoint = 0;
+//std::mutex gvirtus::communicators::EndpointFactory::factory_mutex;
